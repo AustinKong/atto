091d640791d4bc314bf6027163aa089e74e134f3
Austin Kong <konghoitec@gmail.com>
2026-05-23 20:33:14 +0800

    fix: website download button

--- website/src/pages/download/windows.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = async () => {
-  return Response.redirect(
-    "https://github.com/austinkong/atto/releases/latest/download/atto-windows.exe",
-    302
-  )
-};
